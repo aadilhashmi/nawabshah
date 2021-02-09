0fbbe507561ff7bf2c07804e82af0ec1b88cb95d
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-10 00:47:13 +0530

    hiiii

--- src/Components/Linkpages/EP/EP.jsx
+++ src/Components/Linkpages/EP/EP.jsx
@@ -1,54 +1,245 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import "./EP.css";
 
 const EP = () => {
   return (
-    <div>
-      <Card style={{ width: "60rem" }} className="card1">
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title className="tittle">
-            Dr. Mohammad Sanaullah Qaseem (Professor of CSE)
-          </Card.Title>
-          <Card.Text>
-            Dr. Mohammad Sanaullah Qaseem, Professor of CSE, is a versatile and
-            proficient personality with a veteran experience of more than twenty
-            six years, which includes two decades of professional teaching,
-            research and seven years in industry. He completed his B.E. (CSE)
-            from Marathwada University, Aurangabad, in 1992, and did his M.Tech.
-            (CS) (ALCCS) from IETE Hyderabad. He did his Ph. D. (CSE) in January
-            2015, from Acharya Nagarjuna University, Guntur, A.P., on the topic
-            “Context oriented phishing detection system for instant messengers
-            using Domain Ontology and Classification Based Association” under
-            the supervision of Dr. A. Govardhan, Rector and Professor,
-            Department of CSE, JNTU Hyderabad. He has worked in Muscat and
-            Sultanate of Oman as a Software Engineer and Project Leader in an
-            MNC for five years, during which he executed many projects for
-            Oman’s different Ministries and Royal Oman Police. Utilizing his
-            flair as a developer he was instrumental in the complete automation
-            of Examination Branch of Anwarul Uloom College Autonomous during his
-            tenure in AUCCS. He took to academics from 2001 onwards wherein he
-            served renowned institutes like AUCCS, LIET, and Nizam Institute of
-            Engineering and Technology (NIET). During his decade long tenure in
-            NIET he worked in the capacities of Head of CSE, Dean Academics and
-            Principal. It was here that he headed and executed successfully the
-            prestigious TEQIP – II project, sponsored by the World Bank, through
-            the MHRD’s NPIU and SPFU at the State govt. level worth Rs. 4
-            crores. Being an academician he has taught and guided with passion
-            the UG and PG students in projects on Applications and Web
-            Development and their research work as per IEEE standards. He has
-            presented and published a total of 18 research papers in various
-            International journals and International Conferences. He is a
-            lifetime member of ISTE and IETE. Presently he has joined Nawab Shah
-            Alam Khan College of Engineering and Technology since July 2018 as
-            Professor of CSE and Vice Principal Academics. He has also been
-            assigned the additional responsibility as the NAAC Coordinator and
-            heads the IQAC cell.
-          </Card.Text>
-          <Button variant="primary">View Profile</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <React.Fragment>
+      {/* first card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. Mohammad Sanaullah Qaseem (Professor of CSE):
+            </Card.Title>
+            <Card.Img className="img1" src="proff.1.jpg" />
+            <Card.Text>
+              Dr. Mohammad Sanaullah Qaseem, Professor of CSE, is a versatile
+              and proficient personality with a veteran experience of more than
+              twenty six years, which includes two decades of professional
+              teaching, research and seven years in industry. He completed his
+              B.E. (CSE) from Marathwada University, Aurangabad, in 1992, and
+              did his M.Tech. (CS) (ALCCS) from IETE Hyderabad. He did his Ph.
+              D. (CSE) in January 2015, from Acharya Nagarjuna University,
+              Guntur, A.P., on the topic “Context oriented phishing detection
+              system for instant messengers using Domain Ontology and
+              Classification Based Association” under the supervision of Dr. A.
+              Govardhan, Rector and Professor, Department of CSE, JNTU
+              Hyderabad. He has worked in Muscat and Sultanate of Oman as a
+              Software Engineer and Project Leader in an MNC for five years,
+              during which he executed many projects for Oman’s different
+              Ministries and Royal Oman Police. Utilizing his flair as a
+              developer he was instrumental in the complete automation of
+              Examination Branch of Anwarul Uloom College Autonomous during his
+              tenure in AUCCS. He took to academics from 2001 onwards wherein he
+              served renowned institutes like AUCCS, LIET, and Nizam Institute
+              of Engineering and Technology (NIET). During his decade long
+              tenure in NIET he worked in the capacities of Head of CSE, Dean
+              Academics and Principal. It was here that he headed and executed
+              successfully the prestigious TEQIP – II project, sponsored by the
+              World Bank, through the MHRD’s NPIU and SPFU at the State govt.
+              level worth Rs. 4 crores. Being an academician he has taught and
+              guided with passion the UG and PG students in projects on
+              Applications and Web Development and their research work as per
+              IEEE standards. He has presented and published a total of 18
+              research papers in various International journals and
+              International Conferences. He is a lifetime member of ISTE and
+              IETE. Presently he has joined Nawab Shah Alam Khan College of
+              Engineering and Technology since July 2018 as Professor of CSE and
+              Vice Principal Academics. He has also been assigned the additional
+              responsibility as the NAAC Coordinator and heads the IQAC cell.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* second card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              D.r M.K. Nizamuddin (Professor):
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="proff.2.jpg" />
+            <Card.Text>
+              Dr. M.K. Nizamuddin, graduated from OU in 2002 and post graduate
+              from JNTUH in 2008, both with distinction and awarded doctorate by
+              RU in 2017. He has 16 years of academic and research experience.
+              He has published seven (07) research papers in reputed
+              international journals as well as conferences. Out of seven, Two
+              (02) are IEEE publications and One (01) is Springer publication.
+              He is a reviewer of several reputed journals and was technical
+              member of several IEEE conferences. During his tenure as In-charge
+              Computer Centre and In-charge examinations did commendable work
+              and received appreciation letters from employer. As a BOS member
+              of OU recommended several courses which are included in the
+              curriculum and designed syllabus for the courses like Cyber
+              Security, Information Security, Advanced Databases etc. He was
+              invited as adviser for campus networking and as subject expert by
+              some Central Universities.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* third card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. R. Ramesh Reddy M.Tech(Structural) - IIT-Delhi(1976) and Ph.D.
+              - IIT-Delhi(1992).
+            </Card.Title>
+            <Card.Img className="img1" variant="top" src="proff.3.jpg" />
+            <Card.Text>
+              Dr. R. Ramesh Reddy joined OSMANIA UNIVERSITY service in 1979. He
+              worked as Head of Civil Engineering Department from 1998-2000 and
+              served as Chairman of Civil Engg. Board of studies 2001-2003 He
+              was Principal &amp; Member Secretary, Board of Governors and
+              Chairman Academic Council of University College of Engineering,
+              Osmania University from 2003 to 2005. From 2006-2007 he was on
+              deputation as Managing Director of a US based MNC Infobrain a
+              software company. Dr. R. Ramesh Reddy served as the Dean as well
+              as Chairman, faculty of Engineering, Osmania University 2007-2009.
+              Professor of Civil Engineering and served as member of the
+              Governing Body of OU Engineering College also.
+              <br />
+              Presently He is working as Professor of Civil Engineering in Nawab
+              Shah Alam Khan College Of Engineering &amp; Technology, Hyderabad.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* fourth card */}
+
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. B. Srinivas Reddy (Professor of Civil Engineering)
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="" />
+            <Card.Text>
+              Dr. B. Srinivas Reddy joined Nawab Shah Alam Khan College Of
+              Engineering and Technology on 22nd august 2016 as Professor of
+              Civil Engineering and also as Dean, Faculty of Engineering. Dr.
+              Reddy has obtained his B.E(Civil) from Osmania University and also
+              M.E(Structures) from Osmania University. He has joined Osmania
+              university as lecturer in Civil Engg in April 1978 after getting
+              his Master's Degree. He has joined Osmania University as lecturer
+              in Civil Engg in April 1978 after getting his Master's Degree. He
+              has served Osmania University from 1978 April to 2009 March.
+              During that period he held serveral important positions in Osmania
+              University such as I/C Exams, Professor, Head of Civil Enng Depts,
+              Chairman board of studies, special officers(PTDC and PTPG),
+              University I/C Engineering etc. He has published several papers in
+              National and International Conferences, Journals and Conducted
+              Several Seminars and Conferences.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* fifth card  */}
+
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. Mir Moazzam Ali (M.Sc.,PhD(Chemistry), MACS, LMACT, HOD, Dept.
+              of Chemistry){" "}
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="" />
+            <Card.Text>
+              Dr. Mir Moazzam Ali, a greatly respected and sought after guide,
+              has published several research papers in national and
+              international technical journals. A supremely skilled organizer of
+              seminars as well as symposia he has participated in 13 national
+              and international conferences.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+
+      {/* sixth card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. G. Sambasiva Rao (Professor & HOD CSE (B.Tech(CSE),
+              M.Tech(CSE), Ph.D(CSE))
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="" />
+            <Card.Text>
+              Dr. Mir Moazzam Ali, a greatly respected and sought after guide,
+              has published several research papers in national and
+              international technical journals. A supremely skilled organizer of
+              seminars as well as symposia he has participated in 13 national
+              and international conferences.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* seventh card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. Mir Moazzam Ali (M.Sc.,PhD(Chemistry), MACS, LMACT, HOD, Dept.
+              of Chemistry){" "}
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="" />
+            <Card.Text>
+              Dr. Mir Moazzam Ali, a greatly respected and sought after guide,
+              has published several research papers in national and
+              international technical journals. A supremely skilled organizer of
+              seminars as well as symposia he has participated in 13 national
+              and international conferences.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+      {/* eight card */}
+      <div>
+        <Card className="card">
+          <Card.Body>
+            <Card.Title className="EP-tittle">
+              Dr. Mir Moazzam Ali (M.Sc.,PhD(Chemistry), MACS, LMACT, HOD, Dept.
+              of Chemistry){" "}
+            </Card.Title>
+            <Card.Img className="img1" variant="" src="" />
+            <Card.Text>
+              Dr. Mir Moazzam Ali, a greatly respected and sought after guide,
+              has published several research papers in national and
+              international technical journals. A supremely skilled organizer of
+              seminars as well as symposia he has participated in 13 national
+              and international conferences.
+            </Card.Text>
+            <Button className="cardbutton" variant="">
+              View Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </React.Fragment>
   );
 };
 
