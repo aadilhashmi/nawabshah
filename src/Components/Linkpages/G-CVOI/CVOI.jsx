9e4a04944fb8caedb2c05415e0196645139c2ec0
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-15 17:45:25 +0530

    HIIIII

--- src/Components/Linkpages/G-CVOI/CVOI.jsx
+++ src/Components/Linkpages/G-CVOI/CVOI.jsx
@@ -5,12 +5,14 @@ const CVOI = () => {
   return (
     <div className="container">
       <h2 className="head1">
-        <b>
-          <u>Core Values Of Institute</u>
-        </b>
+        <b>CORE VALUES OF INSTITUTE</b>
       </h2>
+      <div className="border"></div>
+      <br />
       <h3 className="head1">Integrity</h3>
-      <h7 className="head1">Defination:</h7>
+      <h7 cl assName="head1">
+        Defination:
+      </h7>
       <p className="para-cvoi">
         Setting the highest ethical standards for self and others in everything
         we do. Our actions, behaviours and conduct must always generate trust
@@ -25,7 +27,7 @@ const CVOI = () => {
             <Accordion.Toggle as={Button} variant="link" eventKey="0">
               <h3>Demonstrable / Observable Attributes:</h3>
             </Accordion.Toggle>
-          <Accordion.Collapse eventKey="0">
+            <Accordion.Collapse eventKey="0">
               <Card.Body>
                 <ul>
                   <li>Is honest and transparent.</li>
@@ -39,13 +41,30 @@ const CVOI = () => {
           </Card>
         </Accordion>
 
+        {/*  */}
+
+        <h2 className="head1">
+          <b>CONTINUOUS LEARNING</b>
+        </h2>
+        <div className="border"></div>
+        <br />
+        <h3 className="head1">Integrity</h3>
+        <h7 cl assName="head1">
+          Defination:
+        </h7>
+        <p className="para-cvoi">
+          Deeply interested in and committed to life-long learning. A passion
+          for enhancing one’s knowledge, skills, awareness and understanding in
+          a variety of fields and subjects.
+        </p>
+
         {/* accordian2 */}
         <Accordion defaultActiveKey="0">
           <Card className="cvoi">
-        
-              <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                Click me!
-              </Accordion.Toggle>   <Accordion.Collapse eventKey="1">
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              Click me!
+            </Accordion.Toggle>{" "}
+            <Accordion.Collapse eventKey="1">
               <Card.Body>Hello! I'm another body</Card.Body>
             </Accordion.Collapse>
           </Card>
